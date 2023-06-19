62051bd96e434faaa56fbb7dd720a0f4b69ef746
Harshad Sharma <harshad@sharma.io>
2023-06-20 02:52:28 +0530

    Cleanup

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 import asyncio
 import random
 
-from socketio.exceptions import BadNamespaceError
 
 from agent import Agent
 
@@ -9,35 +8,22 @@ progress = 0
 agent = Agent(name="hello")
 
 
-@agent.on_interval(seconds=2)
+@agent.on_interval(seconds=1)
 async def timed_job():
     global progress
     try:
-        await agent.emit("progress-too", progress / 100)
-        print(f"Sent: {progress}")
         if progress == 100:
             progress = 0
         elif progress > 100:
             progress = 100
         else:
             progress += random.randint(1, 10)
-    except BadNamespaceError:
-        print("BadNamespaceError")
-
-
-async def main():
-    try:
-        await asyncio.sleep(3)
-        await agent.connect("ws://localhost:8080")
-        await agent.start()
-        while True:
-            await asyncio.sleep(1)
-    except asyncio.exceptions.CancelledError:
-        print("\nStopping...")
-    finally:
-        await agent.stop()
-        await agent.disconnect()
+        progress = min(100, progress)
+        print("Emitting progres: ", progress)
+        await agent.emit("progress-too", progress / 100)
+    except ConnectionError:
+        print("ConnectionError")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(agent.run("ws://localhost:8080"))
